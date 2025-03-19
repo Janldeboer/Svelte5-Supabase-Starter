05ab038f907b66e97c6b826f83b9e4827e4698df
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2025-03-20 00:08:03 +0100

    Vertical positioning fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,6 +19,9 @@
 	})
 </script>
 
-<Navbar {session} />
-
-{@render children()}
+<div class="flex flex-col min-h-screen bg-gray-50">
+	<Navbar {session} />
+	<main class="flex-1 flex flex-col">
+		{@render children()}
+	</main>
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -30,7 +30,7 @@
   }
 </script>
 
-<div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+<div class="flex-1 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-md">
     <h1 class="text-center text-3xl font-extrabold text-gray-900">Welcome Back</h1>
     <p class="mt-2 text-center text-sm text-gray-600">
